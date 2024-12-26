ac5f18b1b1fcfdfd8750138371a33b718f564238
Nicholas Fransisco <nicholasfransisco09@gmail.com>
2024-12-26 17:36:38 +0700

    final: mutasi & user data class

--- app/src/main/java/com/example/fusion_bank/MainActivity.kt
+++ app/src/main/java/com/example/fusion_bank/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.fusion_bank
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
     var db = Firebase.firestore
+
+    var transaksi = ArrayList<Transaksi>()
+    var user = ArrayList<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/fusion_bank/Transaksi.kt
+++ app/src/main/java/com/example/fusion_bank/Transaksi.kt
@@ -1,9 +1,8 @@
 package com.example.fusion_bank
 
 data class Transaksi(
-    var id: String,
     var sender: String,
-    var receiverName: String,
+    var receiver: String,
     var jumlah: Number,
     var berita: String,
     var tanggal: String
